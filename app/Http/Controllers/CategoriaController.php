feec73ece83ff08220504656072ed4936f1ea8ad
selene <selene.rf@hotmail.com>
2021-04-18 21:13:46 +0200

    commit prueba

---
+++ app/Http/Controllers/CategoriaController.php
@@ -0,0 +1,107 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use App\Models\Categoria;
+use Illuminate\Http\Request;
+
+class CategoriaController extends Controller
+{
+    /**
+     * Display a listing of the resource.
+     *
+     * @return \Illuminate\Http\Response
+     */
+    public function index()
+    {
+        //metodo index
+        $categorias=Categoria::orderBy('nombre');
+        return view('categorias.index', compact('categorias'));
+    }
+
+    /**
+     * Show the form for creating a new resource.
+     *
+     * @return \Illuminate\Http\Response
+     */
+    public function create()
+    {
+        return view('categorias.create');
+    }
+
+    /**
+     * Store a newly created resource in storage.
+     *
+     * @param  \Illuminate\Http\Request  $request
+     * @return \Illuminate\Http\Response
+     */
+    public function store(Request $request)
+    {
+        $request->validate([
+            'nombre'=>['required'],
+            'desc'=>['required']
+        ]);
+
+        $categoria = new Categoria();
+        $categoria->nombre = strtoupper($request->nombre);
+        $categoria->desc = $request->desc;
+        //guardamos y regresamos al inicio
+        $categoria->save();
+        return redirect()->route('categorias.index')->with('mensaje', 'Categoria Creada');
+    }
+
+    /**
+     * Display the specified resource.
+     *
+     * @param  \App\Models\Categoria  $categoria
+     * @return \Illuminate\Http\Response
+     */
+    public function show(Categoria $categoria)
+    {
+        return view('categorias.show', compact('categoria'));
+    }
+
+    /**
+     * Show the form for editing the specified resource.
+     *
+     * @param  \App\Models\Categoria  $categoria
+     * @return \Illuminate\Http\Response
+     */
+    public function edit(Categoria $categoria)
+    {
+        return view('categorias.edit', compact('categoria'));
+    }
+
+    /**
+     * Update the specified resource in storage.
+     *
+     * @param  \Illuminate\Http\Request  $request
+     * @param  \App\Models\Categoria  $categoria
+     * @return \Illuminate\Http\Response
+     */
+    public function update(Request $request, Categoria $categoria)
+    {
+        $request->validate([
+            'nombre'=>['required'],
+            'desc'=>['required']
+        ]);
+
+        $categoria->update([
+            'nombre'=>strtoupper($request->nombre),
+            'desc'=>$request->desc
+        ]);
+        return redirect()->route('categorias.index')->with('mensaje', 'Categoria modificada correctamente');
+    }
+
+    /**
+     * Remove the specified resource from storage.
+     *
+     * @param  \App\Models\Categoria  $categoria
+     * @return \Illuminate\Http\Response
+     */
+    public function destroy(Categoria $categoria)
+    {
+        $categoria->delete();
+        return redirect()->route('categorias.index')->with('mensaje', 'Categoria borrada correctamente');
+    }
+}
