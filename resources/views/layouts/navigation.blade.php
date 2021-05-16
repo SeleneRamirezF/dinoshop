b10f40fe48e186d226864015e17f218e9f51bcfa
selene <selene.rf@hotmail.com>
2021-05-16 20:25:49 +0200

    vista inicio

--- resources/views/layouts/navigation.blade.php
+++ resources/views/layouts/navigation.blade.php
@@ -1,4 +1,4 @@
-<nav x-data="{ open: false }" class="bg-white border-b border-gray-100">
+<nav x-data="{ open: false }" class="bg-white border-b border-gray-100" style="background-color:#b1ebaf;">
     <!-- Primary Navigation Menu -->
     <div class="max-w-7xl mx-auto px-4 sm:px-6 lg:px-8">
         <div class="flex justify-between h-16">
@@ -6,7 +6,7 @@
                 <!-- Logo -->
                 <div class="flex-shrink-0 flex items-center">
                     <a href="{{ route('dashboard') }}">
-                        <x-application-logo class="block h-10 w-auto fill-current text-gray-600" />
+                        <img class="block h-20 w-50 fill-current " src="{{asset("storage/img/logotipo.svg")}}" alt="">
                     </a>
                 </div>
 
